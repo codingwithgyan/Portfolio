5e7250ea9af75cf3c4ace981ed962a1588c25db3
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-16 10:37:46 +0530

    .

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,11 @@ export function About()
                 <div className="left">
                     <h1><a> Hi</a> I'M</h1>
                     <h2>GYANESHWAR</h2>
-                    <h4>A Full Stack Web Developer based in India. A creative coder who specializes in MERN stack. I make it my mission to design pixel-perfect websites or applications with optimized code that run blazing fast.</h4>
+                    <h4>An energetic full-stack web developer with a fluid
+understanding of MERN stack. A team
+player, who has an experience leading a team as well as work in a team effectively. Looking forward to working in an organization where he
+can get hands-on experience.
+</h4>
                     <Button variant="contained" style={{backgroundColor:'#ef1932',float:"left",marginTop:"15px"}}>Resume &nbsp; <ArrowCircleDown style={{fontSize:"18px"}} /></Button>
                 </div>
                 <div className="right">
